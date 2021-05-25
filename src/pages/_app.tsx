77927bfdee80b72522c029a47a9bde57b3ce73d2
Fábio de Carvalho <callmefabioo@gmail.com>
2021-05-25 01:32:26 -0300

    feat: update manifest.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>
-          Multistreamers - Watch yout favorites streamers all at once!
+          Multistreamers - Watch your favorites streamers all at once!
         </title>
       </Head>
       <GlobalStyles />
