c5500b128bc9d8b3c8bddea88204a7d3cb21f4b9
Fábio de Carvalho <callmefabioo@gmail.com>
2021-05-27 01:14:45 -0300

    feat: add logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import 'twin.macro';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { SearchInput } from 'components/SearchInput';
 
@@ -11,8 +12,10 @@ export type HeaderProps = {
 export const Header = ({ hideSearchInput, onSearchClick }: HeaderProps) => {
   return (
     <header tw="relative grid items-center grid-cols-3 p-2">
-      <Link href="/">
-        <a>Multistreamers</a>
+      <Link href="/" passHref>
+        <a>
+          <Image src="/logo.png" width={150} height={40} />
+        </a>
       </Link>
 
       {hideSearchInput && (
