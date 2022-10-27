936d45935b7c514708c23e2a956418d6eeb1a71d
Fabio de Carvalho <callmefabioo@gmail.com>
2022-10-26 22:13:36 -0300

    chore: restore react-pwa

--- next.config.js
+++ next.config.js
@@ -5,15 +5,9 @@ const withPWA = require('next-pwa')({
 });
 
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
+  reactStrictMode: true,
   experimental: {
     appDir: true
   }
-};
-
-// module.exports = withPWA({
-//   reactStrictMode: true,
-//   experimental: {
-//     appDir: true
-//   }
-// });
+});
